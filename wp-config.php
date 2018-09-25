804cd9595dab92da2e0f94fc96c33c227149e5b6
dsnid3r <dustin.r.snider@gmail.com>
2018-09-25 11:15:28 -0400

    Fix current site

--- wp-config.php
+++ wp-config.php
@@ -88,7 +88,7 @@ define( 'WP_DEBUG', filter_var( getenv( 'WP_DEBUG' ), FILTER_VALIDATE_BOOLEAN ) 
 /* Multisite */
 define('MULTISITE', true);
 define('SUBDOMAIN_INSTALL', true);
-define('DOMAIN_CURRENT_SITE', 'www.classicpress.net');
+define('DOMAIN_CURRENT_SITE', 'classicpress.net');
 define('PATH_CURRENT_SITE', '/');
 define('SITE_ID_CURRENT_SITE', 1);
 define('BLOG_ID_CURRENT_SITE', 1);
