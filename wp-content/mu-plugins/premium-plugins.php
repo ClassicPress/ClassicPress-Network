a33abf287ad4c003ed49d2886437ed6a3112332f
James Nylen <jnylen@gmail.com>
2018-10-07 12:59:07 -0500

    Remove premium-plugins "helper"
    This may have been a good idea for people working on the main site, but
    not for others (see #8).

--- wp-content/mu-plugins/premium-plugins.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-add_action( 'init', function() {
-	if ( ! class_exists( 'ElementorPro\Plugin' ) ) {
-		wp_die( 'The Elementor Pro plugin is required for this site.  Please contact a site administrator for more information.' );
-	}
-} );
