dc8d6752a9a493722f12139b50b0d015548985dd
Wade <14165147+striebwj@users.noreply.github.com>
2018-10-08 11:28:30 -0400

    Remove role sync to all sites

--- wp-content/plugins/multisite-user-sync/multisite-user-sync.php
+++ wp-content/plugins/multisite-user-sync/multisite-user-sync.php
@@ -2,11 +2,13 @@
 /*
 Plugin Name: Multisite User Sync
 Plugin URI: https://shamimbiplob.wordpress.com/contact-us/
-Description: Multisite User Sync will automatically synchronise users to all sites in multisite. Roles of users will be same on everysite. If Role change in one site it will also synchronise to all site. If new user/site created it will also add to all site/users.
+Description: Multisite User Sync will automatically synchronise users to all sites in multisite. If new user/site created it will also add to all site/users.
 Version: 1.2
 Author: Shamim
 Author URI: https://shamimbiplob.wordpress.com/contact-us/
 License: GPLv2 or later
+
+Note: will no longer sync roles to all sites.
 */
 
 register_activation_hook( __FILE__, 'mus_plugin_activate' );
@@ -24,11 +26,7 @@ function mus_plugin_activate() {
 	remove_action( 'set_user_role', 'mus_add_new_user_role_to_all_sites', 10, 3 );
 	foreach ( $users as $user ) {
 		foreach ( $blogids as $blogid ) {
-			if ( is_array( $user->roles ) && $user->roles ) {
-				add_user_to_blog( $blogid, $user->ID, $user->roles[0] );
-			} else {
-				add_user_to_blog( $blogid, $user->ID, get_blog_option( $blogid, 'default_role', 'subscriber' ) );
-			}
+			add_user_to_blog( $blogid, $user->ID, get_blog_option( $blogid, 'default_role', 'subscriber' ) );	
 		}
 	}
 	add_action( 'set_user_role', 'mus_add_new_user_role_to_all_sites', 10, 3 );
@@ -56,38 +54,9 @@ function mus_add_all_users_to_new_site( $blog_id ) {
 		$users = get_users( array( 'blog_id' => $blogid, 'fields' => 'all_with_meta' ) );
 
 		foreach ( $users as $user ) {
-			if ( is_array( $user->roles ) && $user->roles ) {
-				add_user_to_blog( $blog_id, $user->ID, $user->roles[0] );
-			}
+			add_user_to_blog( $blogid, $user->ID, get_blog_option( $blogid, 'default_role', 'subscriber' ) );	
 		}
 		break;
-	}
-	add_action( 'set_user_role', 'mus_add_new_user_role_to_all_sites', 10, 3 );
-}
-
-/**
- * Assign user Role to all site when change in one site
- *
- * @param  INT $user_id
- * @param  STRING $role - New role
- * @param  ARRAY $old_roles - Old roles
- *
- * @return void
- */
-add_action( 'set_user_role', 'mus_add_new_user_role_to_all_sites', 10, 3 );
-function mus_add_new_user_role_to_all_sites( $user_id, $role, $old_roles ) {
-	global $wpdb;
-
-	if ( ! is_multisite() ) {
-		return;
-	}
-
-	// Query all blogs from multi-site install
-	$blogids = $wpdb->get_col( "SELECT blog_id FROM {$wpdb->base_prefix}blogs" );
-
-	remove_action( 'set_user_role', 'mus_add_new_user_role_to_all_sites', 10, 3 );
-	foreach ( $blogids as $blogid ) {
-		add_user_to_blog( $blogid, $user_id, $role );
 	}
 	add_action( 'set_user_role', 'mus_add_new_user_role_to_all_sites', 10, 3 );
 }
