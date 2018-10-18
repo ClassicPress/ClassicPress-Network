62389da52ac1ae9e0569719309f7816dedfd7f99
Scott Bowler <scottybo@users.noreply.github.com>
2018-10-18 15:52:05 +0100

    Add Google Tag Manager into Elementor Canvas pages (#12)
    * Add Google Tag Manager into Elementor Canvas pages
    
    * Remove old insert of Google Tag Manager (moved to functions.php)
    
    * Add todo

--- wp-content/themes/susty-master/functions.php
+++ wp-content/themes/susty-master/functions.php
@@ -1,4 +1,31 @@
 <?php
+
+/* Add Google Tag Manager into Elementor Canvas pages 
+ * TODO: Look at moving these actions to mu-plugins
+ */
+add_action( 'wp_head', 'custom_add_google_tag_manager_head', 0 );
+add_action( 'elementor/page_templates/canvas/before_content', 'custom_add_google_tag_manager_body' );
+
+function custom_add_google_tag_manager_head () {
+    ?>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-T88732G');</script>
+    <?php
+}
+
+function custom_add_google_tag_manager_body () {
+    ?>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T88732G"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    <?php
+}
+
 /**
  * Susty WP functions and definitions
  *
