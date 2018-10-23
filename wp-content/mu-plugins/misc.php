a0f4b9a29cf6c20d74a5756d99d1b4c8a7bd838c
James Nylen <jnylen@gmail.com>
2018-10-23 16:27:29 -0500

    Add cpnet_subdomain() function

--- wp-content/mu-plugins/misc.php
+++ wp-content/mu-plugins/misc.php
@@ -1,1 +1,16 @@
 <?php
+
+/**
+ * Return the subdomain associated with a given blog ID.
+ *
+ * @param int|null $blog_id The blog ID (defaults to the current blog ID).
+ * @return string  The blog subdomain (e.g. 'www', 'docs', etc).
+ */
+function cpnet_subdomain( $blog_id = null ) {
+	if ( ! $blog_id ) {
+		$blog_id = get_current_blog_id();
+	}
+
+	$domain = get_blog_details( $blog_id )->domain;
+	return preg_replace( '#\..*$#', '', $domain );
+}
