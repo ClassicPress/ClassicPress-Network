7350ffacf95a42b616ddb48cd04c05dcb3986388
James Nylen <jnylen@gmail.com>
2018-10-24 21:05:56 -0500

    Add independent cache buster to theme stylesheets

--- wp-content/themes/classicpress-susty-child/functions.php
+++ wp-content/themes/classicpress-susty-child/functions.php
@@ -1,10 +1,39 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'susty_parent_theme_enqueue_styles' );
+/**
+ * Stylesheet version (cache buster)
+ */
+function cp_susty_get_asset_version() {
+	return '20181024';
+}
 
 /**
- * Enqueue Parent style.css
+ * Enqueue parent theme's style.css
  */
-function susty_parent_theme_enqueue_styles() {
-	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+function cp_susty_enqueue_parent_theme_styles() {
+	wp_enqueue_style(
+		'cp-susty-parent-style',
+		get_template_directory_uri() . '/style.css',
+		[],
+		cp_susty_get_asset_version()
+	);
 }
+add_action( 'wp_enqueue_scripts', 'cp_susty_enqueue_parent_theme_styles' );
+
+/**
+ * Override the way ClassicPress includes the theme's stylesheet so that we can
+ * add our own version string
+ */
+function cp_susty_override_style_css_version( $html, $handle, $href, $media ) {
+	if ( $handle !== 'susty-style' ) {
+		return $html;
+	}
+
+	$ver = cp_susty_get_asset_version();
+	return preg_replace(
+		'#\?ver=[^\'"]+([\'"])#',
+		'?ver=' . $ver . '$1',
+		$html
+	);
+}
+add_filter( 'style_loader_tag', 'cp_susty_override_style_css_version', 10, 4 );
